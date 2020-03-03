59ef8116eda1d40de01cb7b22d0a9670e5cfbf69
45duro <jonathangc.awt@gmail.com>
2020-03-03 17:17:23 -0500

    prueba a medias del sensor qtr archivo para devolver en caso de que no funcione mi nueva idea

--- mainCode/mainCode.ino
+++ mainCode/mainCode.ino
@@ -1,121 +1,101 @@
 #include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
-#include <NewPing.h>
 #include <RTClib.h>
+#include <QTRSensors.h>
+#include <LiquidCrystal_I2C.h>
 
-#define SONAR_NUM     2 // Number of sensors.
-#define MAX_DISTANCE 200 // Maximum distance (in cm) to ping.
-#define PING_INTERVAL 50 // Milliseconds between sensor pings (29ms is about the min to avoid cross-sensor echo).
-#define ITERATIONS     5 // Number of iterations.
-#define numSensors 4
-
+//Configuracion de Sensores
+const uint8_t SensorCount = 4;
+uint16_t sensorValues[SensorCount];
 
 //unidades poara facilitar medida
-unsigned long mitiempo = 0;
-unsigned long pingTimer[SONAR_NUM]; // Holds the times when the next ping should happen for each sensor.
-unsigned int cm[SONAR_NUM];         // Where the ping distances are stored.
-uint8_t currentSensor = 0;          // Keeps track of which sensor is active.
-int posAnterior[SONAR_NUM], posActual[SONAR_NUM];
-
-
-
-int medidaPosicion[2];
-float medidaVelocidad[2];
-
-boolean flags[numSensors], memorias[numSensors];
-
-NewPing sonar[SONAR_NUM] = {     // Sensor object array.
-  NewPing(7, 6, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping.
-  NewPing(8, 9, MAX_DISTANCE),
-  
-};
+unsigned long mitiempo = 0, tiempoInicial = 0, currentTime[SensorCount];
+boolean memoria[SensorCount];
+//objetos
 LiquidCrystal_I2C lcd(0x23,20,4);
+QTRSensors qtr;
+
 
 void setup() {
+
+  //Inicia comunicación
   Serial.begin(115200);
   lcd.init();
   lcd.backlight();
-  
-  pingTimer[0] = millis() + 75;           // First ping starts at 75ms, gives time for the Arduino to chill before starting.
-  for (uint8_t i = 1; i < SONAR_NUM; i++) // Set the starting time for each sensor.
-    pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
+  lcd.print("Calibrando");
+  // configure the sensors y para mejorar con array
+  qtr.setTypeAnalog();
+  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3}, SensorCount);
 
-  //CONVERTIR SENSORES EN ENTRADAS
-  for(byte i = 0; i <= numSensors;i++){
-    pinMode(i+14, INPUT);
+  delay(500);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
+
+  //METER AQUI UN PULSADOR DE CALIBRACION PTE
+  /* analogRead() takes about 0.1 ms on an AVR.
+  // 0.1 ms per sensor * 4 samples per sensor read (default) * 6 sensors
+  // * 10 reads per calibrate() call = ~24 ms per calibrate() call.
+  // Call calibrate() 400 times to make calibration take about 10 seconds.
+  */
+  for (uint16_t i = 0; i < 400; i++)
+  {
+    qtr.calibrate();
   }
+  digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
+
+  /* print the calibration minimum values measured when emitters were on
+  */
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.minimum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+
+  // print the calibration maximum values measured when emitters were on
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.maximum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+  Serial.println();
+  delay(1000);
 
 }
 
 void loop() {
 
-  lectura_Sensor();
-  for(byte i = 0; i < numSensors; i++ ){
-    if(flags[i])
-      memorias[i]=1;
-  }
-
-  
-    
+ //OBTENER VALORES
+ int16_t position = qtr.readLineBlack(sensorValues);
+ int16_t *posicion = &position;
  
+ //IMPRIMIR VALORES
+ for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(sensorValues[i]);
+    Serial.print('\t');
+  }
+  Serial.println(position);
+
+  tiempoInicial = millis();
+  
+  if(posicion >0 && posicion <= qtr.calibrationOn.maximum[0]){
+     currentTime[0]=millis() - tiempoInicial; 
+     memoria[0] = 1;  
+  }
+  else if(posicion >1000 && posicion <= qtr.calibrationOn.maximum[1]+1000){
+    currentTime[1]=millis() - currentTime[0]; 
+     memoria[1] = 1;  
+  }
+  else if(posicion >2000 && posicion <= qtr.calibrationOn.maximum[2]+2000){
+    currentTime[2]=millis() - currentTime[1]; 
+     memoria[2] = 1;  
+  }
+  else if(posicion >3000 && posicion <= qtr.calibrationOn.maximum[3]+3000){
+    currentTime[3]=millis() - currentTime[1]; 
+     memoria[3] = 1;  
+  }
   
   
-}
 
-void echoCheck() { // If ping received, set the sensor distance to array.
-  if (sonar[currentSensor].check_timer())
-    cm[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
-}
-
-void oneSensorCycle() { // Sensor ping cycle complete, do something with the results.
-  // The following code would be replaced with your code that does something with the ping results.
-  int derivada = SONAR_NUM*PING_INTERVAL;
-  float flag =0;
-  //Asignacion de tiempos
-  for(byte i = 0;  i < SONAR_NUM; i++){
-    posActual[i] = cm[i];
-    flag = float(posActual[i]-posAnterior[i])/derivada;
-
-    medidaPosicion[i] = posActual[i];
-    medidaVelocidad[i]= (abs(flag),3);
-    Serial.print("\tpos"); Serial.print(i); Serial.print(": "); Serial.print(posActual[i]);
-    Serial.print("\tVel"); Serial.print(i); Serial.print(": "); Serial.print(abs(flag),3);
-    
-  }
-
-  Serial.println();
-  
-  
-}
-
-
-void medirConRadar(){
-  for (uint8_t i = 0; i < SONAR_NUM; i++) { // Loop through all the sensors.
-    if (millis() >= pingTimer[i]) {         // Is it this sensor's time to ping?
-      pingTimer[i] += PING_INTERVAL * SONAR_NUM;  // Set next time this sensor will be pinged.
-      if (i == 0 && currentSensor == SONAR_NUM - 1) oneSensorCycle(); // Sensor ping cycle complete, do something with the results.
-      sonar[currentSensor].timer_stop();          // Make sure previous timer is canceled before starting a new ping (insurance).
-      currentSensor = i;                          // Sensor being accessed.
-      cm[currentSensor] = 0;                      // Make distance zero in case there's no ping echo for this sensor.
-      sonar[currentSensor].ping_timer(echoCheck); // Do the ping (processing continues, interrupt will call echoCheck to look for echo).
-    }
-    posAnterior[i] = posActual[i];
-  }
-}
-
-void lectura_Sensor(){
- 
-  for(byte i = 0; i < numSensors; i++){
-    //Estas lineas son para borrrar solamente es la prueba
-    if(i == 0 || i == 3)
-      digitalRead(i+14)? flags[i]=0: flags[i]=1 ;
-    else
-      flags[i]=digitalRead(i+14); 
-  }
-
-  for(byte i = 0; i < numSensors; i++){
-    lcd.setCursor(0,i);
-    lcd.print(flags[i]); 
-  }
- 
 }
