157fcfcc61f47f5dd8b6866c3c07d0ac71bda416
45duro <jonathangc.awt@gmail.com>
2020-03-03 22:14:18 -0500

    CRONOMETRO FUNCIONANDO

--- mainCode/mainCode.ino
+++ mainCode/mainCode.ino
@@ -1,101 +1,166 @@
-#include <Wire.h> 
-#include <RTClib.h>
-#include <QTRSensors.h>
+#include "DHT.h"
+#define DHTPIN 17
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+DHT dht(DHTPIN, DHTTYPE);
+float h, t;
+
+#include <SD.h>
+#include <Wire.h>
+File logFile;
+
 #include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x23, 20, 4);
 
-//Configuracion de Sensores
-const uint8_t SensorCount = 4;
-uint16_t sensorValues[SensorCount];
+#include <RTClib.h>
+RTC_DS3231 rtc;
+DateTime now;
 
-//unidades poara facilitar medida
-unsigned long mitiempo = 0, tiempoInicial = 0, currentTime[SensorCount];
-boolean memoria[SensorCount];
-//objetos
-LiquidCrystal_I2C lcd(0x23,20,4);
-QTRSensors qtr;
-
+// Cronómetro
+bool Flag1, Flag2, Flag3, Flag4, Flag5, Flag6;
+String TP1,TP2,TP3,TP4,TP5,TP6;
+unsigned long inicio, finalizado, Ttranscurrido;
 
 void setup() {
 
-  //Inicia comunicación
-  Serial.begin(115200);
+  Serial.begin(9600);
+  Serial.println("Inicializar...");
+  dht.begin();
+  Wire.begin(); // Inicia el puerto I2C
+
   lcd.init();
   lcd.backlight();
-  lcd.print("Calibrando");
-  // configure the sensors y para mejorar con array
-  qtr.setTypeAnalog();
-  qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3}, SensorCount);
 
-  delay(500);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
-
-  //METER AQUI UN PULSADOR DE CALIBRACION PTE
-  /* analogRead() takes about 0.1 ms on an AVR.
-  // 0.1 ms per sensor * 4 samples per sensor read (default) * 6 sensors
-  // * 10 reads per calibrate() call = ~24 ms per calibrate() call.
-  // Call calibrate() 400 times to make calibration take about 10 seconds.
-  */
-  for (uint16_t i = 0; i < 400; i++)
-  {
-    qtr.calibrate();
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
   }
-  digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
 
-  /* print the calibration minimum values measured when emitters were on
-  */
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr.calibrationOn.minimum[i]);
-    Serial.print(' ');
+  Serial.print(F("Iniciando SD ..."));
+  if (!SD.begin(53))  {
+    Serial.println(F("Error al iniciar"));
+    return;
   }
-  Serial.println();
+  Serial.println(F("Iniciado correctamente"));
 
-  // print the calibration maximum values measured when emitters were on
-  for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(qtr.calibrationOn.maximum[i]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  Serial.println();
-  delay(1000);
+
+  pinMode(A0, INPUT_PULLUP); // Boton de inicio
+  pinMode(A1, INPUT_PULLUP); // TP1
+  pinMode(A2, INPUT_PULLUP); // TP2
+  pinMode(A3, INPUT_PULLUP); // TP3
+  pinMode(A4, INPUT_PULLUP); // TP4
+  pinMode(A5, INPUT_PULLUP); // TP5
+  pinMode(A6, INPUT_PULLUP); // TP6/RESET
 
 }
 
+
+
 void loop() {
 
- //OBTENER VALORES
- int16_t position = qtr.readLineBlack(sensorValues);
- int16_t *posicion = &position;
- 
- //IMPRIMIR VALORES
- for (uint8_t i = 0; i < SensorCount; i++)
-  {
-    Serial.print(sensorValues[i]);
-    Serial.print('\t');
-  }
-  Serial.println(position);
+  Sensores();
+  now = rtc.now();
 
-  tiempoInicial = millis();
-  
-  if(posicion >0 && posicion <= qtr.calibrationOn.maximum[0]){
-     currentTime[0]=millis() - tiempoInicial; 
-     memoria[0] = 1;  
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
   }
-  else if(posicion >1000 && posicion <= qtr.calibrationOn.maximum[1]+1000){
-    currentTime[1]=millis() - currentTime[0]; 
-     memoria[1] = 1;  
-  }
-  else if(posicion >2000 && posicion <= qtr.calibrationOn.maximum[2]+2000){
-    currentTime[2]=millis() - currentTime[1]; 
-     memoria[2] = 1;  
-  }
-  else if(posicion >3000 && posicion <= qtr.calibrationOn.maximum[3]+3000){
-    currentTime[3]=millis() - currentTime[1]; 
-     memoria[3] = 1;  
-  }
-  
-  
+  h = dht.readHumidity();
+  t = dht.readTemperature();
 
+
+}
+
+void Sensores() {
+  if ((analogRead(A0) <= 500 ) and (Flag1 == 0) and (Flag2 == 0) and (Flag3 == 0) and (Flag4 == 0) and (Flag5 == 0) and (Flag6 == 0)) {
+    inicio = millis();
+    delay(300); //  por rebote
+    Serial.println("Inicializar...");
+    Flag1 = 1;
+    Serial.print(String("Fecha: ") + now.day() + String("/") + now.month() + String("/") + now.year());
+    Serial.print(String("   Hora: ") + now.hour() + String(":") + now.minute() + String(":") + now.second());
+    Serial.println(String("   Temperatura: ") + float(t) + String("  Humedad: ") + float(h));
+  }
+  if ((analogRead(A1) <= 500) and (Flag1 == 1) )  {
+    finalizado = millis();
+    delay(300); // por rebote
+    MostrarResultado();
+    Flag1 = 0; Flag2 = 1;
+    Serial.println("Flag1...");
+  }
+  if ((analogRead(A2) <= 500) and (Flag2 == 1) )  {
+    finalizado = millis();
+    delay(300); // por rebote
+    MostrarResultado();
+    Flag2 = 0; Flag3 = 1;
+    Serial.println("Flag2...");
+  }
+  if ((analogRead(A3) <= 500) and (Flag3 == 1) )  {
+    finalizado = millis();
+    delay(300); // por rebote
+    MostrarResultado();
+    Flag3 = 0; Flag4 = 1;
+    Serial.println("Flag3...");
+  }
+  if ((analogRead(A4) <= 500) and (Flag4 == 1) )  {
+    finalizado = millis();
+    delay(300); // por rebote
+    MostrarResultado();
+    Flag4 = 0; Flag5 = 1;
+    Serial.println("Flag4...");
+  }
+  if ((analogRead(A5) <= 500) and (Flag5 == 1) )  {
+    finalizado = millis();
+    delay(300); // por rebote
+    MostrarResultado();
+    Flag5 = 0; Flag6 = 1;
+    Serial.println("Flag5...");
+  }
+
+  if ((analogRead(A6) <= 500) and (Flag1 == 0) and (Flag2 == 0) and (Flag3 == 0) and (Flag4 == 0) and (Flag5 == 0) and (Flag6 == 1)) {
+    finalizado = millis();
+    delay(300); // por rebote
+    MostrarResultado();
+    Flag6 = 0;
+    inicio = millis();
+    Serial.println("Flag6... y reset");
+    Grabar();
+  }
+}
+
+void MostrarResultado() {
+  float h, m, s, ms;
+  unsigned long terminado;
+  Ttranscurrido = finalizado - inicio;
+  h = int(Ttranscurrido / 3600000);
+  terminado = Ttranscurrido % 3600000;
+  m = int(terminado / 60000);
+  terminado = terminado % 60000;
+  s = int(terminado / 1000);
+  ms = terminado % 1000;
+  Serial.print("Tiempo transcurrido : ");
+  Serial.println(Ttranscurrido);
+  Serial.print("Tiempo transcurrido detallado: ");
+  Serial.print(m, 0);  Serial.print("m ");
+  Serial.print(s, 0);  Serial.print("s ");
+  Serial.print(ms, 0); Serial.println("ms");
+  Serial.println();
+}
+
+void Grabar() {
+
+  logFile = SD.open("datalog.txt", FILE_WRITE);
+
+  if (logFile) {
+    logFile.print(String("Fecha: ") + now.day() + String("/") + now.month() + String("/") + now.year());
+    logFile.print(String("   Hora: ") + now.hour() + String(":") + now.minute() + String(":") + now.second());
+    logFile.print(String("   Temperatura: ") + float(t) + String("  Humedad: ") + float(h));
+    
+    logFile.close();
+    Serial.println("Guardado el archivo");
+
+  }
+  else {
+    Serial.println("Error al abrir el archivo");
+  }
+  delay(1000);
 }
